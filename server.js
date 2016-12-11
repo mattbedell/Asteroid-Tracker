bfa2fed95c4a4132373692c023daeed8fd8827ed
Matt Bedell <matt@bedell.co>
2016-12-11 11:32:01 -0500

    troubleshoot port conflict

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const logger = require('morgan');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(logger('dev'));
 
-app.use('/api', require('./routes/api'));
+//app.use('/api', require('./routes/api'));
 
 app.listen(PORT, () => console.log(`SERVER LISTENING ON ${PORT}`))
